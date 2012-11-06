0e8089c054162bc6697529524e9f05bfd3ed552b
unknown <dchelm@uc.cl>
2012-11-06 13:06:00 -0400

    Mejora visual en la applicacion usada para mostrar la funcionalidad

--- src/IIC2113/resource/manager/Resource.java
+++ src/IIC2113/resource/manager/Resource.java
@@ -1,6 +1,8 @@
 package IIC2113.resource.manager;
 
+import android.graphics.Color;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
 
 public class Resource implements IResource {
 											//0				1		2				3			4			5
@@ -30,6 +32,8 @@ public class Resource implements IResource {
 	}
 
 	public void cancelConsumption() {
+		ImageButton img = (ImageButton)mainActivity.findViewById(R.id.imageButton1);
+		img.setBackgroundColor(Color.RED);
 		status = 0;
 	}
 
@@ -38,33 +42,47 @@ public class Resource implements IResource {
 	}
 
 	public boolean recieveAction(int action_id,String[] param) {
+		ImageButton img = (ImageButton)mainActivity.findViewById(R.id.imageButton1);
 		switch(action_id){
 			case 0://TAKE A PICTURE
 				if(!this.isAvailable())
+				{
+					img.setBackgroundColor(Color.RED);	
 					return false;
+				}
 				status = 1;
+				img.setBackgroundColor(Color.CYAN);
 				observer.consumptionFinished(this.id, "bin/temp/temp"+pic_counter+".jpg");
 				pic_counter++;
 				status = 0;
 				break;
 			case 1://START RECORDING VIDEO
 				if(!this.isAvailable())
+				{
+					img.setBackgroundColor(Color.RED);	
 					return false;
+				}
+				img.setBackgroundColor(Color.YELLOW);
 				status = 3;
 				break;
 			case 2://STOP RECORDING VIDEO
 				CheckBox checkBox = (CheckBox) mainActivity.findViewById(R.id.checkBox1);
 				if(!checkBox.isChecked()){
+					img.setBackgroundColor(Color.RED);					
 					observer.consumptionFailed(this.id, "DISPONIBILIDAD DE CAMARA CAMBIO DURANTE EJECUCION");
 					return false;
 				}
 				if(status == 3){
 					status = 0;
+					img.setBackgroundColor(Color.BLUE);
 					observer.consumptionFinished(this.id, "bin/temp/temp"+vid_counter+".mpeg");
 					vid_counter++;
 				}
 				else
+				{
+					img.setBackgroundColor(Color.RED);	
 					return false;
+				}
 				break;
 		}
 		return true;
